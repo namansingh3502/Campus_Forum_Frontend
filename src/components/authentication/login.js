aed4ec1e3d7288ad4864366aa7682c04ccd8106a
namansigh3502 <namansingh0305@gmail.com>
2022-03-07 16:11:22 +0530

    env

--- src/components/authentication/login.js
+++ src/components/authentication/login.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState} from "react";
 import axios from "axios";
 import {useLocation, useNavigate} from "react-router-dom";
-import process from "process/browser";
 
 export default function Login() {
   let navigate = useNavigate();
