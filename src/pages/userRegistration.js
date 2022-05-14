5aa4b1f9e0515c80d57fa09906f1cadf3937ed2e
namansingh3502 <namansingh0305@gmail.com>
2022-05-14 19:33:37 +0530

    added validation to registration form

--- src/pages/userRegistration.js
+++ src/pages/userRegistration.js
@@ -17,6 +17,19 @@ const DEPARTMENT = [
   { value: "MECH", label: "Mechanical" },
 ];
 
+const LABEL = (props) => {
+  return (
+    <label className="block mx-1">
+      <span>{props.label}</span>
+      {props.required && <span className={"text-red-600 mx-1"}>*</span>}
+      <span className={"text-red-600"}>{props.message}</span>
+    </label>
+  );
+};
+
+const FIELD_CLASS =
+  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400";
+
 const UserRegistration = () => {
   const {
     register,
@@ -45,14 +58,14 @@ const UserRegistration = () => {
     console.log("data ", data);
 
     axios
-      .post("/api/register", { data }, {})
+      .post("/api/auth/register", { data }, {})
       .then((res) => {
         if (res.status === 200) {
-          console.log("user created");
+          alert("user created");
         }
       })
       .catch((errors) => {
-        console.log(errors.response);
+        alert(errors.response.data.msg);
       });
   }
 
@@ -62,7 +75,6 @@ const UserRegistration = () => {
         <h1 className="font-bold text-center text-4xl text-yellow-500">
           SJBIT CSE<span className="text-blue-500 ml-2">Forum</span>
         </h1>
-
         <div className={"text-center mt-4"}>
           <p className="font-bold text-xl">User Registration</p>
         </div>
@@ -70,54 +82,49 @@ const UserRegistration = () => {
         <form className={"py-2"} onSubmit={handleSubmit(createUser)}>
           <div className="flex flex-wrap w-full">
             <div className={"w-full sm:w-1/3 px-1 py-2"}>
-              <label className="block ml-1">
-                <span>Username</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>
-                  {errors.username?.message}
-                </span>
-              </label>
+              <LABEL
+                label={"Username"}
+                message={errors.username?.message}
+                required={true}
+              />
               <input
                 type="text"
                 placeholder="Username"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
+                className={FIELD_CLASS}
                 {...register("username", { required: "field required" })}
               />
             </div>
             <div className={"w-full sm:w-1/3 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Password</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>
-                  {errors.password1?.message}
-                </span>
-              </label>
+              <LABEL
+                label={"Password"}
+                message={errors.password1?.message}
+                required={true}
+              />
               <input
                 type="password"
                 placeholder="**********"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
-                {...register("password1")}
+                className={FIELD_CLASS}
+                {...register("password1", {
+                  required: "field required",
+                  minLength: { value: 8, message: "min length 8" },
+                })}
               />
+              {console.log("error ", errors)}
             </div>
             <div className={"w-full sm:w-1/3 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Password</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>
-                  {errors.password2?.message}
-                </span>
-              </label>
+              <LABEL
+                label={"Password"}
+                message={errors.password2?.message}
+                required={true}
+              />
               <input
                 type="password"
                 placeholder="**********"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
-                {...register("password2", { required: "field required" })}
+                className={FIELD_CLASS}
+                {...register("password2", {
+                  required: "field required",
+                  minLength: { value: 8, message: "min length 8" },
+                })}
               />
             </div>
 
@@ -132,92 +139,93 @@ const UserRegistration = () => {
             </span>
 
             <div className={"w-full sm:w-1/3 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>First Name</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>
-                  {errors.firstName?.message}
-                </span>
-              </label>
+              <LABEL
+                label={"First Name"}
+                message={errors.firstName?.message}
+                required={true}
+              />
               <input
                 type="text"
                 placeholder="First Name"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
+                className={FIELD_CLASS}
                 {...register("firstName", { required: "field required" })}
               />
             </div>
             <div className={"w-full sm:w-1/3 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Middle Name</span>
-              </label>
+              <LABEL
+                label={"Middle Name"}
+                message={errors.middleName?.message}
+                required={false}
+              />
+
               <input
                 type="text"
                 placeholder="Middle Name"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
+                className={FIELD_CLASS}
                 {...register("middleName")}
               />
             </div>
             <div className={"w-full sm:w-1/3 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Last Name</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>
-                  {errors.lastName?.message}
-                </span>
-              </label>
+              <LABEL
+                label={"Last Name"}
+                message={errors.lastName?.message}
+                required={true}
+              />
               <input
                 type="text"
                 placeholder="Last Name"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
+                className={FIELD_CLASS}
                 {...register("lastName", { required: "field required" })}
               />
             </div>
-
             <div className={"w-full sm:w-1/2 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Email</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>{errors.email?.message}</span>
-              </label>
+              <LABEL
+                label={"Email"}
+                message={errors.email?.message}
+                required={true}
+              />
               <input
                 type="email"
-                placeholder="example@example.com"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
-                {...register("email", { required: "field required" })}
+                placeholder="example@sjbit.edu.in"
+                className={FIELD_CLASS}
+                {...register("email", {
+                  required: "field required",
+                  pattern: {
+                    value: /^[A-Z\d._%+-]+@sjbit.edu.in$/i,
+                    message: "invalid email address",
+                  },
+                })}
               />
             </div>
             <div className={"w-full sm:w-1/2 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Phone</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>{errors.phone?.message}</span>
-              </label>
+              <LABEL
+                label={"Phone"}
+                message={errors.phone?.message}
+                required={true}
+              />
               <input
                 type="text"
                 placeholder="Phone"
-                className={
-                  "w-full px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 ring-1 ring-slate-400"
-                }
-                {...register("phone", { required: "field required" })}
+                className={FIELD_CLASS}
+                {...register("phone", {
+                  required: "field required",
+                  pattern: {
+                    value: /^[0-9]{10}$/i,
+                    message: "Invalid Phone no.",
+                  },
+                })}
               />
             </div>
             <div className={"w-full sm:w-1/2 px-1 py-2"}>
-              <label className="block mx-1">
-                <span>Gender</span>
-                <span className={"text-red-600 mx-1"}>*</span>
-                <span className={"text-red-600"}>{errors.gender?.message}</span>
-              </label>
+              <LABEL
+                label={"Gender"}
+                message={errors.gender?.message}
+                required={true}
+              />
               <Controller
                 name="gender"
                 control={control}
+                rules={{ required: "field required" }}
                 render={({ field }) => (
                   <ReactSelect
                     className={
@@ -226,21 +234,23 @@ const UserRegistration = () => {
                     isClearable
                     {...field}
                     options={GENDER}
+                    onChange={(e) => {
+                      field.onChange(e);
+                    }}
                   />
                 )}
               />
             </div>
             <div className={"w-full sm:w-1/2 px-1 py-2"}>
-              <label className="block ml-1">
-                Department
-                <span className={"text-red-600 ml-1"}>
-                  *{errors.dapartment?.message}
-                </span>
-              </label>
-
+              <LABEL
+                label={"Department"}
+                message={errors.department?.message}
+                required={true}
+              />
               <Controller
                 name="department"
                 control={control}
+                rules={{ required: "field required" }}
                 render={({ field }) => (
                   <ReactSelect
                     className={
@@ -249,6 +259,7 @@ const UserRegistration = () => {
                     isClearable
                     {...field}
                     options={DEPARTMENT}
+                    onChange={(e) => field.onChange(e)}
                   />
                 )}
               />
